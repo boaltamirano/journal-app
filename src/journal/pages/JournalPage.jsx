6f27153f13d8fa2165a04c9ce1d3e5412463839d
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-04-24 22:39:45 -0500

    feat: initial view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad praesentium nesciunt molestiae temporibus blanditiis error explicabo porro quisquam laboriosam laudantium inventore enim eligendi hic ducimus, eveniet, at qui nemo aspernatur.</Typography>
+            {/* <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad praesentium nesciunt molestiae temporibus blanditiis error explicabo porro quisquam laboriosam laudantium inventore enim eligendi hic ducimus, eveniet, at qui nemo aspernatur.</Typography> */}
+            <NothingSelectedView />
         </JournalLayout>
     )
 }
