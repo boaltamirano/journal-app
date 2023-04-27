1cf08de30ae8d7b43009f7eb972bfd20e958d4a7
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-04-26 23:23:59 -0500

    feat: Implement form validation registar

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -2,11 +2,26 @@ import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from "react-router-dom"
 import { AuthLayout } from "../layout"
+import { useForm } from "../../hooks"
+
+const formData = {
+    email:       'omar@google.com',
+    password:    '123456',
+    displayName: 'Omar Altamirano'
+}
 
 export const RegisterPage = () => {
+
+    const { displayName, email, password, onInputChange, formState } = useForm(formData);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <AuthLayout title="Register">
-            <form>
+            <form onSubmit={ onSubmit }>
                 <Grid container>
                     <Grid item xs={12} sx={{mt:2}}>
                         <TextField
@@ -14,6 +29,9 @@ export const RegisterPage = () => {
                             type="text"
                             placeholder="Username"
                             fullWidth
+                            name="displayName"
+                            value={displayName}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{mt:2}}>
@@ -22,6 +40,9 @@ export const RegisterPage = () => {
                             type="email"
                             placeholder="email@google.com"
                             fullWidth
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{mt:2}}>
@@ -30,11 +51,18 @@ export const RegisterPage = () => {
                             type="password"
                             placeholder="password"
                             fullWidth
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
                         <Grid item xs={12}>
-                            <Button variant='contained' fullWidth>
+                            <Button 
+                                type="submit"
+                                variant='contained' 
+                                fullWidth
+                            >
                                 Register
                             </Button>
                         </Grid>
