c1dd3fecb167588037381699bad8ebfad5879163
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-05-01 00:01:16 -0500

    feat: Funcionality for delete notes

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useMemo, useRef } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
-import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { DeleteOutline, SaveOutlined, UploadOutlined } from "@mui/icons-material"
 import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import Swal from "sweetalert2"
 import 'sweetalert2/dist/sweetalert2.css'
 
 import { useForm } from "../../hooks"
 import { ImageGallery } from "../components"
-import { setActiveNote, startSaveNote, startUploadingFiles } from "../../store/journal"
+import { setActiveNote, startDeletingNote, startSaveNote, startUploadingFiles } from "../../store/journal"
 
 export const NoteView = () => {
 
@@ -42,6 +42,10 @@ export const NoteView = () => {
         if (target.files === 0) return;
 
         dispatch( startUploadingFiles( target.files ));
+    }
+     
+    const onDelete = () => {
+        dispatch(startDeletingNote());
     }
 
     return (
@@ -105,6 +109,18 @@ export const NoteView = () => {
                     onChange={onInputChange}
                 />
             </Grid>
+
+            <Grid container justifyContent='end'>
+                <Button
+                    onClick={ onDelete }
+                    sx={{ mt: 2 }}
+                    color="error"
+                >
+                    <DeleteOutline />
+                    Delete
+                </Button>
+            </Grid>
+
             <ImageGallery images={ note.imageUrls }/>
         </Grid>
     )
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,6 @@
-import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { collection, deleteDoc, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase";
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setPhotosToActivateNote, setSaving, updateNote } from "./";
+import { addNewEmptyNote, deleteNoteById, savingNewNote, setActiveNote, setNotes, setPhotosToActivateNote, setSaving, updateNote } from "./";
 import { fileUpload, loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
@@ -11,9 +11,10 @@ export const startNewNote = () => {
         const { uid } = getState().auth;
 
         const newNote = {
-            title: '',
-            body: '',
-            date: new Date().getTime(),
+            title: "",
+            body: "",
+            imageUrls: [],
+            date: new Date().getTime()
         }
 
         const newDoc = doc(collection( FirebaseDB, `${uid}/journal/notes` ));
@@ -67,5 +68,17 @@ export const startUploadingFiles = ( files = [] ) => {
         const photosUrls = await Promise.all( fileUploadPromises );
 
         dispatch( setPhotosToActivateNote( photosUrls ));
+    }
+}
+
+export const startDeletingNote = () => {
+    return async (dispatch, getState) => {
+        const { uid } = getState().auth;
+        const { active:note } = getState().journal;
+
+        const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
+        await deleteDoc( docRef );
+
+        dispatch( deleteNoteById(note.id));
     }
 }
