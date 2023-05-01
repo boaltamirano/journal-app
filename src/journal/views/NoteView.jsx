6fda8a1ccecc2c77fae96aa4693cbe84ac57e2d8
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-04-30 20:42:28 -0500

    feat: Notification to updated note using sweetalert2

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,16 +1,20 @@
+import { useEffect, useMemo } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
 import { SaveOutlined } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography } from "@mui/material"
-import { ImageGallery } from "../components"
-import { useDispatch, useSelector } from "react-redux"
+import Swal from "sweetalert2"
+import 'sweetalert2/dist/sweetalert2.css'
+
 import { useForm } from "../../hooks"
-import { useEffect, useMemo } from "react"
+import { ImageGallery } from "../components"
 import { setActiveNote, startSaveNote } from "../../store/journal"
 
 export const NoteView = () => {
 
     const dispatch = useDispatch();
 
-    const { active: note } = useSelector(state => state.journal);
+    const { active:note, messageSaved, isSaving } = useSelector(state => state.journal);
     const { body, title, date, onInputChange, formState } = useForm(note);
 
     const dateString = useMemo(() => {
@@ -21,6 +25,12 @@ export const NoteView = () => {
     useEffect(() => {
         dispatch(setActiveNote(formState));
     }, [formState]);
+
+    useEffect(() => {
+        if(messageSaved.length > 0) {
+            Swal.fire('Note updated', messageSaved, 'success');
+        }
+    }, [messageSaved]);
 
     const onSaveNote = () => {
         dispatch( startSaveNote() );
@@ -40,6 +50,7 @@ export const NoteView = () => {
             </Grid>
             <Grid item>
                 <Button
+                    disabled={isSaving}
                     onClick={ onSaveNote }
                     color="primary"
                     sx={{ padding: 2 }}
